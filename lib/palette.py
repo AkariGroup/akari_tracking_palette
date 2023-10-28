d8c604128087dd775e6ef035fb4da1945530f8e6
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2023-10-28 21:59:41 +0900

    add comments

--- lib/palette.py
+++ lib/palette.py
@@ -20,7 +20,9 @@ class RectRoi(object):
         self.p2: Tuple[float, float] = p2
 
     def is_pos_in_rect(self, pos: Tuple[float, float]) -> bool:
-        # rectは四角形の4つの頂点を表すリスト。
+        """
+        posが矩形エリア内に存在するか確認する。
+        """
         point = Feature(geometry=Point(pos))
         polygon = Polygon(
             [
@@ -41,6 +43,9 @@ class CircleRoi(object):
         self.radius: float = radius
 
     def is_pos_in_circle(self, pos: Tuple[float, float]) -> bool:
+        """
+        posが円形エリア内に存在するか確認する。
+        """
         distance = math.sqrt((pos[0] - self.p1[0]) ** 2 + (pos[1] - self.p1[1]) ** 2)
         if distance <= self.radius:
             return True
@@ -54,12 +59,21 @@ class RoiList(object):
         self.circle: List[CircleRoi] = []
 
     def add_rect(self, p1, p2):
+        """
+        矩形エリアをroiリストに追加する。
+        """
         self.rect.append(RectRoi(p1, p2))
 
     def add_circle(self, p1, radius):
+        """
+        円形エリアをroiリストに追加する。
+        """
         self.circle.append(CircleRoi(p1, radius))
 
     def reset(self):
+        """
+        roiを初期化する。
+        """
         self.rect.clear()
         self.circle.clear()
 
@@ -97,6 +111,9 @@ class RoiPalette(object):
             self.load_roi(roi_path)
 
     def load_roi(self, path: str) -> None:
+        """
+        saveしたroiをpathから取得し、初期状態として使用する。
+        """
         if not os.path.isfile(path):
             print(f"ROI setting file path {path} is not available.")
             return
@@ -119,6 +136,9 @@ class RoiPalette(object):
                                 print(f"ROI file id:{id} circle type wrong")
 
     def save_roi(self) -> None:
+        """
+        roiをjsonファイルにsaveする。
+        """
         current_time = datetime.now()
         file_name = current_time.strftime("%Y%m%d_%H%M%S.json")
         file_path = "roi_json/" + file_name
@@ -143,41 +163,68 @@ class RoiPalette(object):
             json.dump(roi_dict, json_file, indent=4)
 
     def set_mode(self, mode: str) -> bool:
+        """
+        矩形描画と円の描画を切り替える。
+        """
         if mode == "rectangle" or mode == "circle":
             self.mode = mode
             return True
         return False
 
     def set_roi_id(self, id: int) -> bool:
+        """
+        現在のroiのIDを切り替える。
+        """
         if 0 <= id < self.MAX_ROI_ID:
             self.cur_roi_id = id
             return True
         return False
 
     def set_tracklets(self, tracklets) -> None:
+        """
+        trackletをセットする。
+        """
         self.tracklets = tracklets
 
     def reset(self) -> None:
+        """
+        roiを初期化する。
+        """
         for roi in self.roi:
             roi.reset()
 
     def point_to_pos(self, point: Tuple[int, int]) -> Tuple[float, float]:
+        """
+        bird_frame上の座標をロボットから見た3次元位置に変換する。
+        """
         pos_x =  -1 *( point[0] - self.FRAME_WIDTH/2) / self.FRAME_WIDTH * (self.X_RANGE)
         pos_z = (-point[1] + self.FRAME_HEIGHT) / self.FRAME_HEIGHT * (self.Z_RANGE)
         return (pos_x, pos_z)
 
     def point_diff_to_pos_diff(self, diff: float) -> float:
+        """
+        bird_frame上の座標の差分をロボットから見た3次元位置の差分に変換する。
+        """
         return diff / self.FRAME_HEIGHT * self.Z_RANGE
 
     def pos_diff_to_point_diff(self, diff: float) -> float:
+        """
+        ロボットから見た3次元位置の差分をbird_frame上の座標の差分に変換する。
+        """
         return diff / self.Z_RANGE * self.FRAME_HEIGHT
 
     def pos_to_point(self, pos: Tuple[float, float]) -> Tuple[int, int]:
+        """
+        ロボットから見た3次元位置をbird_frame上の座標に変換する。
+        """
         point_x = int( -1 * pos[0] / self.X_RANGE * self.FRAME_WIDTH + self.FRAME_WIDTH/2)
         point_y = self.FRAME_HEIGHT - int(pos[1] / self.Z_RANGE * self.FRAME_HEIGHT)
         return (point_x, point_y)
 
     def create_bird_frame(self) -> np.ndarray:
+        """
+        bird_frameを作成する。
+        """
         # ウィンドウを作成
         cv2.namedWindow(self.window_name)
         cv2.setMouseCallback(self.window_name, self.draw_shape)
@@ -223,6 +270,9 @@ class RoiPalette(object):
         return frame
 
     def add_tracklet_to_frame(self, frame:np.ndarray) -> np.ndarray:
+        """
+        bird_frame上にtrackletの情報を追加する。
+        """
         if self.tracklets is None:
             return frame
         for tracklet in self.tracklets:
@@ -268,6 +318,9 @@ class RoiPalette(object):
         return frame
 
     def draw_fov(self,frame:np.ndarray) -> np.ndarray:
+        """
+        AKARIのヘッドの向きに応じてFOVを描画する。
+        """
         center = int(frame.shape[1] / 2)
         alpha = self.fov / 2
         yaw = self.joints.get_joint_positions()["pan"]
@@ -329,6 +382,9 @@ class RoiPalette(object):
         return frame
 
     def draw_frame(self) -> None:
+        """
+        bird_frameを描画する。
+        """
         frame = self.bird_eye_frame.copy()
         frame = self.draw_fov(frame)
         frame = self.add_tracklet_to_frame(frame)
@@ -404,8 +460,10 @@ class RoiPalette(object):
         )
         cv2.imshow("palette", frame)
 
-    # マウスのコールバック関数
     def draw_shape(self, event, x, y, flags, param) -> None:
+        """
+        bird_frame上に矩形、円形を描画する。
+        """
         self.cur_ix = x
         self.cur_iy = y
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -429,6 +487,9 @@ class RoiPalette(object):
         self.draw_frame()
 
     def is_point_in_roi(self, id: int, pos: Tuple[float, float]) -> bool:
+        """
+        bird_frame上の座標が指定されたroiの内部にあるかを判定して返す。
+        """
         if not 0 <= id < self.MAX_ROI_ID:
             return False
         for rect in self.roi[id].rect:
@@ -448,6 +509,9 @@ class OakdTrackingYoloWithPalette(OakdTrackingYolo):
         tracklets: List[Any],
         roi_palette: RoiPalette,
     ) -> None:
+        """
+        roi_paletteの情報を元に、認識結果の枠の色を変えて描画する。
+        """
         if frame is not None:
             frame = cv2.resize(
                 frame,
